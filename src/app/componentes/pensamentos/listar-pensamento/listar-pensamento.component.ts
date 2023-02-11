1e3060a9babb96fad68a047ce54030c02502365a
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-02-11 10:32:56 -0300

    05. Comunicação com o Backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,3 +1,4 @@
+import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,24 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
-  listaPensamento = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente Pai',
-      modelo: 'modelo3',
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Angular',
-      modelo: 'modelo2',
-    },
-    {
-      conteudo:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, perferendis adipisci quod illum vero facilis nobis in voluptas eveniet est nihil expedita, dolorum provident dicta, veritatis tenetur odio architecto deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum placeat aperiam dignissimos repudiandae aut cum distinctio dolores nam? Quas deleniti assumenda aliquid amet',
-      autoria: 'Angular',
-      modelo: 'modelo1',
-    },
-  ];
+  listaPensamento: Pensamento[] = [];
 
   constructor() {}
 
